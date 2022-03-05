7fa6dca56ea7bf5ac6cf932b0b97b4023864bacd
Simbarashe_Mhlanga <simba@tgpdc.com>
2022-03-05 14:35:50 +0200

    second tutorial concepts mainly on controllers

--- index.php
+++ index.php
@@ -1,13 +1,53 @@
 <?php
 
 require_once("vendor/autoload.php");
-
 $f3=Base::instance();
 
-$f3->route('GET /',
-    function() {
-        echo 'Hello, world!';
+
+class AppController{ //these functions run before and after main and about class
+
+    function beforeroute(){
+
+        echo 'before routing- ';
+
     }
-);
+
+    function afterroute(){
+
+        echo ' after routing- ';
+
+    }
+}
+
+
+class Main extends AppController{
+
+    function render(){
+
+        echo 'Hello, world';
+    }
+
+    function sayhello(){
+
+        echo 'Hello , babe';
+    }
+}
+
+
+class AboutPage extends AppController{
+
+    
+
+    function render(){
+
+        echo 'This is about page';
+    }
+
+}
+
+
+$f3->route('GET /','Main->render');
+$f3->route('GET /hello','Main->sayhello');
+$f3->route('GET /about','AboutPage->render');
 
 $f3->run();
